4d3710b3972983b6e31fc16a29f78ef8deee4460
aldrin163 <aldrinjenson@gmail.com>
2020-05-20 06:30:51 +0530

    First

---
+++ android/app/src/main/java/com/book_o_rama_with_splash/MainActivity.java
@@ -0,0 +1,15 @@
+package com.book_o_rama_with_splash;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "book_o_rama_with_splash";
+  }
+}
